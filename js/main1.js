4595270c5620448caba9d899ca51156dcbe52448
erthemile <79584700+erthemile@users.noreply.github.com>
2021-02-27 15:40:57 +0600

    Add files via upload

---
+++ js/main1.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+  $('a[href^="#"]').click(function() {
+    var elementClick = $(this).attr("href");
+    var destination = $(elementClick).offset().top;
+    jQuery("html:not(:animated), body:not(:animated)").animate(
+      { scrollTop: destination },
+      800
+    );
+    return false;
+  });
+  $(".reviews").slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    fade: false,
+    cssEase: "linear",
+    adaptiveHeight: true
+  });
+
+  $(".callback-btn").click(function() {
+    $(".callback-form").toggleClass("hide");
+  });
+
+  $(".show-callback").click(function() {
+    $(".callback-form").removeClass("hide");
+  });
+});
+
+$(document).click(function(e) {
+  if (
+    $(e.target).closest($(".callback-form, .callback-btn, .show-callback"))
+      .length > 0
+  ) {
+    return;
+  }
+  $(".callback-form").addClass("hide");
+});
+
+$('img[src="img/i-cross.svg"]').click(function() {
+  $(".callback-form").addClass("hide");
+});
